1b66cc57046a4569236807b6be46855f5faf1612
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-09-11 22:25:14 +0300

    Update ai_api_connect.ts

--- src/utils/api/ai_api_connect.ts
+++ src/utils/api/ai_api_connect.ts
@@ -6,4 +6,4 @@ export const openai = new OpenAI({
   apiKey: "",
   baseURL: `http://localhost:${process.env.G4F_PORT}/v1`,
 });
-export const TEXT_AI_MODELS = ["gpt-5","gpt-4o", "gpt-4.1", "gpt-4", "gpt-4o-mini", "gpt-3.5-turbo"];
+export const TEXT_AI_MODELS = ["gpt-4o", "gpt-4.1", "gpt-4", "gpt-4o-mini", "gpt-3.5-turbo"];
